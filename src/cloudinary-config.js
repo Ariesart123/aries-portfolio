52a8d618e515148ce090ffed3a2a502d6e569cd5
Arieso <athrvakumkar97@gmail.com>
2026-08-30 16:28:30 +0530

    Delete src directory

--- src/cloudinary-config.js
+++
@@ -1,8 +0,0 @@
-export const cloudinaryConfig = {
-  cloudName: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
-  uploadPreset: import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
-};
-
-export const cloudinaryConfigured = Boolean(
-  cloudinaryConfig.cloudName && cloudinaryConfig.uploadPreset
-);
